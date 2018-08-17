c28ab0f0521f59d19cc4e28d6bd895c4ebd5f86a
Răzvan C. Rădulescu <razcore.art@gmail.com>
2018-08-17 06:48:12 +0300

    Bump version in setup.py
    Closes #1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 setup(
     name='bpsrender',
-    version='0.1.2',
+    version='0.1.3',
     description='Blender Power Sequencer Renderer',
     long_description=readme(),
     classifiers=[
